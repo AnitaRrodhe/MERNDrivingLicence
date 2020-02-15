b6787d5d440f2a28856915da54ca48686a8adcda
hurma <hurma.hasanaj@epitech.eu>
2020-02-15 18:31:04 -0500

    db connection

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
-var express=require('express');
-var app=express();
-const bodyParser= require('body-parser')
+const express=require('express');
+const app=express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://localhost:27042/driver-license-db',function (err, client){
+	if (err) throw err
+
+	var db = client.db('driver-license-db') 
+
+	db.collection('test').find().toArray(function (err, result){
+		if (err) throw err
+		
+		console.log(result)
+	})
+})
 
 
 app.get('/', function (req, res) {
-   res.send('test');
+	res.send('test');
 })
 
 
